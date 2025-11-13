3b63eeb27d86406605a556ef9c789ae21abdf135
Cainã Antunes <caina497@gmail.com>
2025-11-13 15:51:40 -0300

    Fix: string connection format

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using BackEndAPI.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("BackEndAPIContext");
+
+if (connectionString != null && connectionString.StartsWith("postgres://"))
+{
+    // Converte postgres://user:pass@host:port/db -> Host=...;Username=...;Password=...;Database=...;SSL Mode=Require;Trust Server Certificate=true
+    var uri = new Uri(connectionString);
+    var userInfo = uri.UserInfo.Split(':');
+    connectionString =
+        $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+}
+
 builder.Services.AddDbContext<BackEndAPIContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("BackEndAPIContext") ?? throw new InvalidOperationException("Connection string 'BackEndAPIContext' not found.")));
+
 
 // Add services to the container.
 
