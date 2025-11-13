f3acd8847cf6cb4cfc3a51b13552073dc17e28f4
Cainã Antunes <caina497@gmail.com>
2025-11-13 17:26:46 -0300

    conection

--- Program.cs
+++ Program.cs
@@ -2,37 +2,39 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using BackEndAPI.Data;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<BackEndAPIContext>(options =>
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<BackEndAPIContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("BackEndAPIContext") ?? throw new InvalidOperationException("Connection string 'BackEndAPIContext' not found.")));
-
-
-// Add services to the container.
+} 
+else
+{
+    builder.Services.AddDbContext<BackEndAPIContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionStrings__DefaultConnection") ?? throw new InvalidOperationException("Connection string 'ConnectionStrings__DefaultConnection' not found.")));
+}
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();    
+}
+
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI();    
 
 app.Run();
